409cd3ca57f79da1c52cc023849d01d4e3695489
lucassJDK <lucassJDK@gmail.com>
2025-04-21 00:21:32 +0000

    subindo atividades básica(if/else/elseif/foreach

---
+++ atividadesBasicas/PegarIdsFraudulentos/PegarIdsFraudulentos/Program.cs
@@ -0,0 +1,7 @@
+﻿string [] ids = ["B123","C234","A345","C15","B177","G3003","C235","B179"];
+
+foreach(string id in ids) {
+    if(id.StartsWith("B")) {
+        Console.WriteLine(id);
+    }
+}
---
+++ atividadesBasicas/exFahrenheitParaCelsius/exFahrenheitCelsius/Program.cs
@@ -0,0 +1,2 @@
+﻿int fahrenheit = 94;
+Console.WriteLine($"{fahrenheit}F em celsius é:{(fahrenheit - 32) * 5/9 }");
